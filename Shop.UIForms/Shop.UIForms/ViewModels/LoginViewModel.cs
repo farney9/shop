8495d8d8e8d13a4d8de5f43ea41b42941043eba8
farney9 <farney9@gmail.com>
2019-04-28 14:16:58 -0500

    Show Products on Xamarin forms (ProductViewModel)

--- Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -1,6 +1,7 @@
 ﻿namespace Shop.UIForms.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
+    using Shop.UIForms.Views;
     using System;
     using System.Windows.Input;
     using Xamarin.Forms;
@@ -45,10 +46,13 @@
 
             }
 
-            await Application.Current.MainPage.DisplayAlert(
-                "Ok",
-                "Fuck yeah!!!",
-                "Accept");
+            //await Application.Current.MainPage.DisplayAlert(
+            //    "Ok",
+            //    "Fuck yeah!!!",
+            //    "Accept");
+
+            MainViewModel.GetInstance().Products = new ProductsViewModel();
+            await Application.Current.MainPage.Navigation.PushAsync(new ProductsPage());
         }
     }
 }
