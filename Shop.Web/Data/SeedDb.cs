e6f49f357ac04ac6b366c9a670ce04a86c60df44
farney9 <farney9@gmail.com>
2019-04-30 16:35:37 -0500

    Add Roles. implement Methods on UserHelper class (CheckRoleAsync,AddUserToRoleAsync,IsUserInRoleAsync) add user farney to role Admin in SeeDB.cs

--- Shop.Web/Data/SeedDb.cs
+++ Shop.Web/Data/SeedDb.cs
@@ -24,6 +24,10 @@
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            await this.userHelper.CheckRoleAsync("Admin");
+            await this.userHelper.CheckRoleAsync("Customer");
+
+            //Add User
             var user = await this.userHelper.GetUserByEmailAsync("farney9@gmail.com");
             if (user == null)
             {
@@ -41,8 +45,17 @@
                 {
                     throw new InvalidOperationException("Could not create the user in seeder");
                 }
+
+                await this.userHelper.AddUserToRoleAsync(user, "Admin");
             }
 
+            var isInRole = await this.userHelper.IsUserInRoleAsync(user, "Admin");
+            if (!isInRole)
+            {
+                await this.userHelper.AddUserToRoleAsync(user, "Admin");
+            }
+
+            //Add Products
             if (!this.context.Products.Any())
             {
                 this.AddProduct("iPhone X", user);
